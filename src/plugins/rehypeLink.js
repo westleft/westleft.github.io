726a982a92af865c7c2ae67ec5ea152d624a05b5
佐料 <78782311+westleft@users.noreply.github.com>
2025-05-15 17:26:58 +0800

    fix: http error

--- src/plugins/rehypeLink.js
+++ src/plugins/rehypeLink.js
@@ -4,7 +4,13 @@ import { visit } from 'unist-util-visit'
 export function rehypeLink() {
   return (tree) => {
     visit(tree, { tagName: 'a' }, (node, index, parent) => {
-      const isExternal = node.properties.href.startsWith('http')
+      const href = node.properties.href
+      // 確保內部鏈接使用 HTTPS
+      if (href.startsWith('http://seasoning.dev')) {
+        node.properties.href = href.replace('http://', 'https://')
+      }
+
+      const isExternal = href.startsWith('http')
       if (isExternal) {
         node.properties = {
           ...node.properties,
